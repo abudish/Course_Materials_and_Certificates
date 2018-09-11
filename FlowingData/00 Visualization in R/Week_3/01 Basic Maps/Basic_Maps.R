252de64d990ea8f467d805f41dcb3cd8be915275
Andrey Budish <andreybudish@gmail.com>
2018-09-11 15:45:02 +0300

    Some more files related to FlowingData R data vis course

---
+++ FlowingData/00 Visualization in R/Week_3/01 Basic Maps/Basic_Maps.R
@@ -0,0 +1,26 @@
+# Load location data
+costcos <- read.csv("http://projects.flowingdata.com/tut/data/costcos-geocoded.csv", sep=",")
+
+head(costcos)
+
+# Symbols plot
+plot(costcos$Longitude, costcos$Latitude)
+
+# Install package if needed
+install.packages("maps", dependencies=TRUE)
+ 
+# Load maps package
+library(maps)
+
+# Map of points
+map(database="state", col="#cccccc")
+
+symbols(costcos$Longitude, costcos$Latitude, bg="#e2373f", fg="#ffffff", lwd=0.5, circles=rep(1, length(costcos$Longitude)), inches=0.03, add=TRUE)
+
+# Useless connecting lines
+map(database="state", col="#cccccc")
+costcos.o <- costcos[order(costcos$Zip.Code, decreasing=FALSE),]
+lines(costcos.o$Longitude, costcos.o$Latitude, lwd=0.3, col="red")
+points(costcos.o$Longitude, costcos.o$Latitude, pch=21, col="red", cex=0.2)
+
+
